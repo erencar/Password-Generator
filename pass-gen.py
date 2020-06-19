24e1911f54dd483436d0e91f0046b6bb3fa8e74a
lotusexpeditor <51518492+lotusexpeditor@users.noreply.github.com>
2020-06-19 21:33:29 +0300

    Check Input for Runtime Exceptions
    Possible runtime exceptions weren't checked gracefully. This pull request fixes that.

--- pass-gen.py
+++ pass-gen.py
@@ -10,6 +10,11 @@ def passgen(length):
         result= "".join(random.choice(chars) for _ in range(length))
         return result
 
+while True:
+        try:
+                length=int(input('How many characters in your password? = '))
+                break
+        except ValueError:
+                print("Input wasn't an integer. Try again...")
 
-length=int(input('How many characters in your password? = '))
 print(passgen(length))
